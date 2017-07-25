63bdfd2987f590e94cb27f45f0e659293abcf87d
Teocci Kseek-PC-Ubuntu <teocci@yandex.com>
2017-07-25 19:42:58 +0900

    Update arduino .ino file

--- Arduino Code/Serial_Echo/Serial_Echo.ino
+++ Arduino Code/Serial_Echo/Serial_Echo.ino
@@ -1,12 +1,18 @@
- void setup()  
- {  
-  Serial.begin(9600);  
- }  
- void loop()  
- {  
-  if(Serial.available())  
-  {  
-   char c = Serial.read();  
-   Serial.print(c);  
-  }  
- }  
+#include <Arduino.h>
+
+int LED1 = 10;
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(LED1, OUTPUT);
+}
+
+void loop() {
+    if (Serial.available()) {
+        char c = Serial.read();
+        Serial.print(c);
+        digitalWrite(LED1, HIGH); // Turn on LED1
+        delay(200);
+        digitalWrite(LED1, LOW); // Turn off LED1
+    }
+}
